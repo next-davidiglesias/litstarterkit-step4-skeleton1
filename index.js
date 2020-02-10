3a69a439b1cc8baaf5590f7e52a3ea2a5744d03c
next-davidiglesias <david.iglesias.zavala.next@bbva.com>
2020-02-10 20:28:57 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 import {asyncReplace} from 'lit-html/directives/async-replace.js';
 import { render, html } from "lit-html";
 
-const wait = (t) => new Promise((resolve) => setTimeout(resolve, t));
-async function* countUp() {
-  let i = 0;
-  while (true) {
-    yield i++;
-    await wait(1000);
-  }
-}
+
 
 render(html`
-  Count: <span>${asyncReplace(countUp())}</span>.
+  Count: <span>${asyncReplace()}</span>.
 `, document.body);
 
